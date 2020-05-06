373cf1c7b79602c68a82f078fb5c06a6c6081d2c
youmakemefeellucky <38502195+youmakemefeellucky@users.noreply.github.com>
2020-05-06 22:28:33 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+  $(".expand").click(function() {
+    $('html, body').animate({
+        scrollTop: $(this).offset().top -60
+    }, 500);
+    $(this).next().slideToggle('slow');
+});
+  $(".ok").click(function(){
+      $('html, body').animate({
+          scrollTop: $(this).parent().parent().before().offset().top -100
+      }, 500);
+      $(this).parent().parent().hide('slow');
+  });
+});
