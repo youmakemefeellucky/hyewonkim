625f0de41296242533935c318794d1278f81d885
youmakemefeellucky <38502195+youmakemefeellucky@users.noreply.github.com>
2020-11-20 01:50:28 +0100

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,10 @@
 $(document).ready(function() {
 
+
+
   // Show & hide welcome text, circle on scroll
   var lastScrollTop = 0;
-  $(window).scroll(function() {
+  $(document).scroll(function() {
     var scrollTop = $(this).scrollTop();
     if (scrollTop > lastScrollTop) {
       $('#bonjour').hide();
@@ -13,15 +15,49 @@ $(document).ready(function() {
       $('#circle').fadeIn(800);
       $('.works').css('color','#bfbfbf');
     }
-
   });
 
-  // Scroll to WORKS
-  var y = window.innerHeight/2;
-  $('.works').click(function() {
-    $('html, body').animate({scrollTop: $('#p1').offset().top -y}, 'slow');
-    $('#circle').css('display','none');
-    $('#bonjour').css('display','none');
+
+
+  // Show & hide sidebar
+  $(document).scroll(function() {
+    var y = $(this).scrollTop();
+    if (y > 300) {
+      $('.sidebar').fadeIn();
+    } else {
+      $('.sidebar').fadeOut();
+    }
   });
+
+
+
+  // Sidebar (refactoring required !)
+  var observer1 = new IntersectionObserver(function(entries) {
+  	if(entries[0].isIntersecting === true) {
+      $('#side1').css('color', '#FFEF64');
+    } else {
+      $('#side1').css('color', '#bfbfbf');
+    }
+  }, { threshold: [0] });
+
+  var observer2 = new IntersectionObserver(function(entries) {
+    if(entries[0].isIntersecting === true) {
+      $('#side2').css('color', '#FFEF64');
+    } else {
+      $('#side2').css('color', '#bfbfbf');
+    }
+  }, { threshold: [0] });
+
+  var observer3 = new IntersectionObserver(function(entries) {
+    if(entries[0].isIntersecting === true) {
+      $('#side3').css('color', '#FFEF64');
+    } else {
+      $('#side3').css('color', '#bfbfbf');
+    }
+  }, { threshold: [0] });
+
+  observer1.observe(document.querySelector("#p1-1"));
+  observer2.observe(document.querySelector("#p2"));
+  observer3.observe(document.querySelector("#p3"));
 
 });
